1d65a8568144ae4f055bf56934a58fe45e3abd70
Deepali Garg <hdgarg@gmail.com>
2018-06-26 18:26:17 -0700

    completes the 6ees6ees6ees sprint with all tests passing

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,10 @@
-class HoneyMakerBee {
-  // TODO..
-};
+class HoneyMakerBee extends Bee {
+  constructor(age, job, honeyPot) {
+    super(age, job)
+    this.age = 10
+    this.honeyPot = 0
+    this.job = 'make honey'
+  }
+  makeHoney() { this.honeyPot++}
+  giveHoney() { this.honeyPot--}
+}
